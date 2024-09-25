f633eed48dcfeaa2b405979fe03e743325bd5060
maxon2024-star <bigztbmstu@gmail.com>
2024-09-25 08:39:37 +0000

    lab 3-4 2 changed

--- lab2_rust/rand/src/main.rs
+++ lab2_rust/rand/src/main.rs
@@ -1,45 +1,29 @@
-extern crate rand;
+use rand::Rng; // Импортируем библиотеку rand для генерации случайных чисел
+use std::env;
 
-use rand::Rng; // Не забудьте добавить rand в зависимости вашего Cargo.toml
-
-struct GenRandom {
-    count: usize,
-    min: i32,
-    max: i32,
-    generated: usize,
-}
-
-impl GenRandom {
-    fn new(count: usize, min: i32, max: i32) -> Self {
-        GenRandom {
-            count,
-            min,
-            max,
-            generated: 0,
-        }
-    }
-}
-
-impl Iterator for GenRandom {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.generated < self.count {
-            let mut rng = rng::thread_rng().gen_range(self.min..self.max);// Включает границы
-            self.generated += 1;
-            Some(rng)
-        } else {
-            None
-        }
-    }
-}
-
-fn gen_random(count: usize, min: i32, max: i32) -> GenRandom {
-    GenRandom::new(count, min, max)
+fn gen_random(count: usize, min: i32, max: i32) -> impl Iterator<Item = i32> {
+    let mut rng = rand::thread_rng(); // Создаем генератор случайных чисел
+    (0..count).map(move |_| rng.gen_range(min..=max)) // Генерируем случайные числа в заданном диапазоне
 }
 
 fn main() {
-    let random_numbers: Vec<_> = gen_random(5, 1, 3).collect();
+    let args: Vec<String> = env::args().skip(1).collect(); // Пропускаем первый аргумент (имя программы)
+
+    if args.len() != 3 {
+        eprintln!("Использование: {} <количество> <минимум> <максимум>", args[0]);
+        return;
+    }
+
+    let count: usize = args[0].parse().expect("Некорректное количество");
+    let min: i32 = args[1].parse().expect("Некорректное минимальное значение");
+    let max: i32 = args[2].parse().expect("Некорректное максимальное значение");
+
+    if min > max {
+        eprintln!("Минимальное значение должно быть меньше или равно максимальному.");
+        return;
+    }
+
+    let random_numbers: Vec<i32> = gen_random(count, min, max).collect();
     for number in random_numbers {
         println!("{}", number);
     }
